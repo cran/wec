e1505b511b587bef8e38742f96beac44fc505c55
Rense Nieuwenhuis <rense.nieuwenhuis@sofi.su.se>
2016-11-16 00:15:38 +0000

    version 0.3

--- R/contr.wec.R
+++ R/contr.wec.R
@@ -1,15 +1,14 @@
 contr.wec <-
-function(x, ref)
+function(x, omitted)
 {
     frequencies <- table(x)
     n.cat       <- length(table(x))
-    ref         <- which(levels(x) == ref)
-    
-    new.contrasts <- contr.treatment(n.cat, base=ref)
-    new.contrasts[ref,] <- -1 * frequencies[-ref] / frequencies[ref]
-    
-    colnames(new.contrasts) <- names(frequencies[-ref])
-    
-    contrasts(x) <- new.contrasts
-    return(x)
+    omitted         <- which(levels(x) == omitted)
+
+    new.contrasts <- contr.treatment(n.cat, base=omitted)
+    new.contrasts[omitted,] <- -1 * frequencies[-omitted] / frequencies[omitted]
+
+    colnames(new.contrasts) <- names(frequencies[-omitted])
+
+    return(new.contrasts)
 }
